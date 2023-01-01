8f976bbbbe570a566cad412c26e6d8fad177e95e
RAGBouch94 <121772546+RAGBouch94@users.noreply.github.com>
2023-01-01 20:16:31 -0300

    Add files via upload

---
+++ 2. Detalle_Partidos.sql
@@ -0,0 +1,6 @@
+SELECT   country.name AS COUNTRY, league.name AS LEAGUE, season AS SEASON, date AS DATE, Equipo_Local.team_long_name AS LOCAL_TEAM, Equipo_visita.team_long_name AS AWAY_TEAM, home_team_goal AS LOCAL_GOAL, away_team_goal AS AWAY_GOAL
+FROM Match 
+INNER JOIN Country ON match.country_id = country.id
+INNER JOIN League ON match.country_id = league.country_id
+INNER JOIN Team AS Equipo_Visita ON match.away_team_api_id = Equipo_Visita.team_api_id 
+INNER JOIN Team AS Equipo_Local ON match.home_team_api_id = Equipo_Local.team_api_id
